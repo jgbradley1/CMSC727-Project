e4800910594f565f73267ebad233586525261d0e
Rob Argue <rob7045713@gmail.com>
2013-05-08 22:00:15 -0400

    more report, plus plots

--- code/Elman Code/run_time_experiments.m
+++ code/Elman Code/run_time_experiments.m
@@ -1,12 +1,15 @@
 cols = {'pwr_rp','pwr_elm'};
 sizes = [25:25:200];
-times = zeros(1,2);
+times = zeros(3,2);
 result = zeros(8,2);
 
 for s = 1:8
   net_size = sizes(s);
-  power_rp; result(s,1) = max(tr.time);
-  power_elman; result(s,2) = max(tr.time);
+  for run = 1:3
+    power_rp; times(run,1) = max(tr.time);
+    power_elman; times(run,2) = max(tr.time);
+  end
+  result(s, :) = mean(times);
 end
 
 save('power_times.mat','cols','sizes','result');
